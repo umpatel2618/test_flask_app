53f6f26cb244e55361b91bb68476ccd1d1874709
uttampatel-btc <uttam.patel@tntra.io>
2022-06-02 18:39:02 +0530

    [Update] test s

--- tests.py
+++ tests.py
@@ -3,5 +3,5 @@ from hello import app
 
 with app.test_client() as c:
     response = c.get('/')
-    assert response.data == b'Hello World! lets test'
+    assert response.data == b'Hello World! lets test CI/CD'
     assert response.status_code == 200
